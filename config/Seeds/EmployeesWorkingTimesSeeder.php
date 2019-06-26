341d9206d40ddc4d69bc5a27f760e9678941c7a4
Ventsislav Nicolov <venci.nicolov@gmail.com>
2019-06-26 16:03:10 +0300

    Create seeder for working times

--- config/Seeds/EmployeesWorkingTimesSeeder.php
+++ config/Seeds/EmployeesWorkingTimesSeeder.php
@@ -5,6 +5,12 @@ use Phinx\Seed\AbstractSeed;
 
 class EmployeesWorkingTimesSeeder extends AbstractSeed
 {
+        public function getDependencies()
+    {
+        return [
+            'EmployeesProjectsSeeder'
+        ];
+    }
     /**
      * Run Method.
      *
@@ -15,6 +21,20 @@ class EmployeesWorkingTimesSeeder extends AbstractSeed
      */
     public function run()
     {
+        $foreignKeys = $this->adapter->fetchAll('select id from employees_projects');
+        $faker = \Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 5; $i++) {
+            $data[] = [
+                'employee_project_id' => $foreignKeys[array_rand($foreignKeys)]['id'],
+                'working_time' => $faker->time('H:00:00'),
+                'working_overtime' => $faker->time('H:00:00'),
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ];
+        }
 
+        $table = $this->table('working_times');
+        $table->insert($data)->save();
     }
 }
